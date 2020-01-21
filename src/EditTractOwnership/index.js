5717bdea3f7280d70fcbcc7f2af9baa962e86d3f
Jonathan Birkholz <rookieone@gmail.com>
2020-01-21 00:50:18 -0600

    Add EditTractOwnership component

---
+++ src/EditTractOwnership/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EditTractOwnership = ({ value = [], onChange = () => { } }) => {
+  return (
+    <h2>
+      TODO
+    </h2>
+  );
+};
+
+export default EditTractOwnership;
