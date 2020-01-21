cd219dcce295e2ed097588dfda496d0ffb90aa62
Jonathan Birkholz <rookieone@gmail.com>
2020-01-21 00:52:03 -0600

    Add layout to App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Col, Container, Jumbotron, Row } from 'react-bootstrap'
+import uuidv4 from 'uuid/v4'
+
+import 'bootstrap/dist/css/bootstrap.css';
 import './App.css';
+
+import EditTractOwnership from './EditTractOwnership'
+import Icon from './Icon'
+
+const tractOwnerships = [{
+  id: uuidv4(),
+  owner: "Luke Skywalker",
+  interest: 0.5,
+  lease: "Tatooine Lease",
+  npris: [{
+    id: uuidv4(),
+    owner: "Leia Organa",
+    interest: 0.45
+  }, {
+    id: uuidv4(),
+    owner: "Han Solo",
+    interest: 0.15
+  }]
+}]
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Jumbotron>
+            <h1>Landdox Code Challenge <Icon icon="smile" /></h1>
+          </Jumbotron>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <EditTractOwnership value={tractOwnerships} />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
