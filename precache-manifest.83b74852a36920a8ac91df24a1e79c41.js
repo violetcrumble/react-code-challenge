1ea87012e7992c66b1b6e142f0c9876f5b7554f3
Bonnie Mellott <bonniemellott@gmail.com>
2020-01-25 19:58:04 -0500

    Updates

---
+++ precache-manifest.83b74852a36920a8ac91df24a1e79c41.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dafff58f2a98b5a21c69f1b1c079039c",
+    "url": "/react-code-challenge/index.html"
+  },
+  {
+    "revision": "b74f757739966b538b25",
+    "url": "/react-code-challenge/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "7b268785f9ddaaf0a16a",
+    "url": "/react-code-challenge/static/css/main.24cdcb49.chunk.css"
+  },
+  {
+    "revision": "b74f757739966b538b25",
+    "url": "/react-code-challenge/static/js/2.2aff35bf.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/react-code-challenge/static/js/2.2aff35bf.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b268785f9ddaaf0a16a",
+    "url": "/react-code-challenge/static/js/main.12c57f3a.chunk.js"
+  },
+  {
+    "revision": "c2e9ebbbbe02ef27f045",
+    "url": "/react-code-challenge/static/js/runtime-main.2052a404.js"
+  },
+  {
+    "revision": "13cf316859669e81d3e35573878ceb2d",
+    "url": "/react-code-challenge/static/media/primary_logo.13cf3168.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-code-challenge/precache-manifest.3e933be26ce4b44e32b6a91059637897.js"
+  "/react-code-challenge/precache-manifest.83b74852a36920a8ac91df24a1e79c41.js"
 );
 
 self.addEventListener('message', (event) => {
